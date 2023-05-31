af60278c1c41a7b5c99032ec5ed13a0fdf1fae71
Sinhleba <sinhleba001@gmail.com>
2023-05-31 22:14:45 +0700

    config menu

--- app/src/main/java/com/sinhle/restaurantapp/MainActivity.java
+++ app/src/main/java/com/sinhle/restaurantapp/MainActivity.java
@@ -105,14 +105,36 @@ public class MainActivity extends AppCompatActivity {
             databaseHandler.addBook(new Book());
             databaseHandler.addBook(new Book());
             databaseHandler.addBook(new Book());
-            //đồ uống
-            databaseHandler.addDrink(new Drink("Trà sữa truyền thống",0,40000));
-            databaseHandler.addDrink(new Drink("Cà phê đen",0,50000));
-            databaseHandler.addDrink(new Drink("Cà phê sữa",0,30000));
-            databaseHandler.addDrink(new Drink("Trà sữa Thái xanh",0,40000));
-            databaseHandler.addDrink(new Drink("Trà sữa trân châu đường đen",0,50000));
-            databaseHandler.addDrink(new Drink("Hồng Trà",0,30000));
+            //menu
+            databaseHandler.addDrink(new Drink("Trà sữa truyền thống",0,20000));
+            databaseHandler.addDrink(new Drink("Cam ép",0,20000));
+            databaseHandler.addDrink(new Drink("Redbull",0,15000));
+            databaseHandler.addDrink(new Drink("Sữa chua",0,10000));
+            databaseHandler.addDrink(new Drink("Cà phê đen",0,12000));
+            databaseHandler.addDrink(new Drink("Cà phê sữa",0,15000));
+            databaseHandler.addDrink(new Drink("Trà sữa Thái xanh",0,25000));
+            databaseHandler.addDrink(new Drink("Trà sữa trân châu đường đen",0,25000));
             databaseHandler.addDrink(new Drink("Cappuccino",0,50000));
+            databaseHandler.addDrink(new Drink("Nước ngọt Sting",0,12000));
+
+            databaseHandler.addDrink(new Drink("Khăn lạnh",0,3000));
+            databaseHandler.addDrink(new Drink("Trà đá",0,3000));
+
+            databaseHandler.addDrink(new Drink("Bánh mì Thịt chả",0,20000));
+            databaseHandler.addDrink(new Drink("Hủ tiếu mì",0,30000));
+            databaseHandler.addDrink(new Drink("Cơm sườn",0,25000));
+            databaseHandler.addDrink(new Drink("Cơm sườn bì chả",0,400000));
+            databaseHandler.addDrink(new Drink("Bánh mì bò kho",0,30000));
+            databaseHandler.addDrink(new Drink("Phở bò",0,40000));
+            databaseHandler.addDrink(new Drink("Bì thêm",0,5000));
+            databaseHandler.addDrink(new Drink("Sườn thêm",0,20000));
+            databaseHandler.addDrink(new Drink("Chả thêm",0,20000));
+            databaseHandler.addDrink(new Drink("Lẩu Thái đặc biệt",0,350000));
+
+
+
+
+
 
         }catch(Exception e){
             e.printStackTrace();
